4245346d2683f256b8630bd303ecd964d3a3c7be
Quoc Hau <hautlq20403c@st.uel.edu.vn>
2022-12-05 18:59:21 +0700

    [comment]

---
+++ src/todoapp.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+
+export default function ToDolist({ toDolist, onAdd, onComplete }) {
+  const [value, setValue] = useState("");
+
+  const listDoing = toDolist.filter((e) => !e.isComplete);
+  const listDone = toDolist.filter((e) => e.isComplete);
+  const _onAdd = (e) => {
+    onAdd(value.trim());
+    setValue("");
+  };
+  const onKeyUp = (e) => {
+    if (e.key === "Enter") _onAdd();
+  };
+  
+  return (
+    <>
+      <div className="abc">
+        <input
+          onKeyUp={onKeyUp}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          type="text"
+          placeholder="Công việc"
+        />
+        <button onClick={_onAdd} disabled={!value.trim()}>
+          Thêm
+        </button>
+      </div>
+
+      <div className="congviec">
+        <div className="doing">
+          <h1>Công Việc Chưa Hoàn Thành</h1>
+          {listDoing.map((e) => (
+            <ToDoItem onComplete={onComplete} key={e.id} {...e} />
+          ))}
+        </div>
+        <div className="done">
+          <h1>Công Việc Đã Hoàn Thành</h1>
+          {listDone.map((e) => (
+            <ToDoItem key={e.id} {...e} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+const ToDoItem = ({ id, name, isComplete, onComplete }) => {
+  return (
+    <div className={isComplete ? "isComplete" : ""}>
+      <div className={"check"}>
+        <h3 className="name">{name}</h3>
+        <button
+          onClick={() => {
+            onComplete(id);
+          }}>
+          V
+        </button>
+      </div>
+    </div>
+  );
+};
